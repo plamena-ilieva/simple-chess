c36f584ba80dfb958892ef2ca09360ea5a64f7bd
plamena-ilieva <plamena.ilieva117@gmail.com>
2021-12-27 12:28:07 +0200

    Add initial files

---
+++ cmake-build-debug/CMakeFiles/simple_chess.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_chess.dir/main.cpp.obj"
+  "simple_chess.exe"
+  "simple_chess.exe.manifest"
+  "simple_chess.lib"
+  "simple_chess.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
